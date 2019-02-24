75c639d433de1c8f59a8ccab4902c48431c14b4d
Sergei <sergei.visotsky@gmail.com>
2019-02-24 08:24:48 +0200

    SQL format

--- studies/joins.sql
+++ studies/joins.sql
@@ -1,45 +1,40 @@
 use sakila;
-SELECT 
-    c.city,
-    c.city_id,
-    c.last_update,
-    co.country,
-    co.last_update,
-    a.address,
-    a.district
-FROM
-    city c
-        LEFT OUTER JOIN
-    country co ON c.country_id = co.country_id
-        RIGHT OUTER JOIN
-    address a ON c.city_id = a.city_id;
+SELECT c.city,
+       c.city_id,
+       c.last_update,
+       co.country,
+       co.last_update,
+       a.address,
+       a.district
+FROM city cŌ
+       LEFT OUTER JOIN
+     country co ON c.country_id = co.country_id
+       RIGHT OUTER JOIN
+     address a ON c.city_id = a.city_id;
 
 
-SELECT
-    fa.film_id,
-    fa.last_update,
-    f.title,
-    f.description,
-    f.release_year,
-    f.rental_rate,
-    f.length,
-    f.replacement_cost,
-    f.special_features,
-    fc.category_id,
-    ft.title,
-    act.first_name,
-    act.last_name,
-    ft.description
-FROM
-    film_actor fa
-        LEFT OUTER JOIN
-    film f ON fa.film_id = f.film_id
-        LEFT OUTER JOIN
-    film_category fc ON fc.film_id = f.film_id
-        LEFT OUTER JOIN
-    actor act ON act.actor_id = fa.actor_id
-        LEFT OUTER JOIN
-    film_text ft ON ft.film_id = f.film_id
-WHERE
-        f.last_update = '2006-02-15 05:03:42'
+SELECT fa.film_id,
+       fa.last_update,
+       f.title,
+       f.description,
+       f.release_year,
+       f.rental_rate,
+       f.length,
+       f.replacement_cost,
+       f.special_features,
+       fc.category_id,
+       ft.title,
+       act.first_name,
+       act.last_name,
+       ft.description
+FROM film_actor fa
+       LEFT OUTER JOIN
+     film f ON fa.film_id = f.film_id
+       LEFT OUTER JOIN
+     film_category fc ON fc.film_id = f.film_id
+       LEFT OUTER JOIN
+     actor act ON act.actor_id = fa.actor_id
+       LEFT OUTER JOIN
+     film_text ft ON ft.film_id = f.film_id
+WHERE f.last_update = '2006-02-15 05:03:42'
 GROUP BY f.title;
--- studies/procedures.sql
+++ studies/procedures.sql
@@ -1,18 +1,15 @@
 USE sakila;
 DELIMITER //
-CREATE PROCEDURE insert_actor(
-  in p_first_name varchar(45),
-  in p_last_name varchar(45),
-  in p_last_update timestamp)
+CREATE PROCEDURE insert_actor(IN p_first_name VARCHAR(45),
+                              IN p_last_name VARCHAR(45),
+                              IN p_last_update TIMESTAMP)
   READS SQL DATA
 BEGIN
   IF p_first_name OR p_last_name IS NOT NULL THEN
-    INSERT INTO actor(
-      first_name,
-      last_name,
-      last_update)
-    VALUES(
-            p_first_name,
+    INSERT INTO actor(first_name,
+                      last_name,
+                      last_update)
+    VALUES (p_first_name,
             p_last_name,
             p_last_update);
   END IF;
